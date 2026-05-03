4e96e4ca6241a7bec32c9023ba5a9f65ab634a55
pranaeshv <pranaeshv18082004@gmail.com>
2026-05-03 09:31:41 +0530

    redirect changed

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,12 @@ import ProtectedRoute from './components/ProtectedRoute'
 import Tailor from './pages/Tailor'
 import Preview from './pages/Preview'
 
-
-
-
-
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={
           <ProtectedRoute><Dashboard /></ProtectedRoute>
@@ -26,12 +23,9 @@ export default function App() {
         <Route path="/builder/:id" element={
           <ProtectedRoute><Builder /></ProtectedRoute>
         } />
-
         <Route path="/tailor/:id" element={
           <ProtectedRoute><Tailor /></ProtectedRoute>
         } />
-
-
         <Route path="/preview/:id" element={
           <ProtectedRoute><Preview /></ProtectedRoute>
         } />
